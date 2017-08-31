095453b07e6a0dff05b3b6a5188dbf5e6851899c
Dio Brando <7iscool77@gmail.com>
2017-08-31 15:01:01 +0200

    some unfinished js

---
+++ duelingfates.js
@@ -0,0 +1,23 @@
+// start script
+
+// get vars by id
+
+// def //
+var def1 = document.getElementById("item1def");
+var def2 = document.getElementById("item2def");
+var def3 = document.getElementById("item3def");
+var def4 = document.getElementById("item4def");
+var def5 = document.getElementById("item5def");
+var def6 = document.getElementById("item6def");
+// atk //
+var atk1 = document.getElementById("item1atk");
+var atk2 = document.getElementById("item2atk");
+var atk3 = document.getElementById("item3atk");
+var atk4 = document.getElementById("item4atk");
+var atk5 = document.getElementById("item5atk");
+var atk6 = document.getElementById("item6atk");
+
+// vars for items
+function testButton() {
+  document.getElementById("test").innerHTML = "Item 1 is " + def1 + "and " + atk1 + ".";
+}
