96fe428701f0b73e77c2d430a50290cfa6c05b10
Dio Brando <7iscool77@gmail.com>
2017-09-15 11:08:40 +0200

    added .value
    lifesaver

--- duelingfates.js
+++ duelingfates.js
@@ -22,30 +22,29 @@ var atk6;
 function testButton() {
 // set vars again on button press
 
-  var flying = document.getElementById("amUphill");
-  var uphill = document.getElementById("amFlying");
+  var flying =  amFlying.value;
+  var uphill = amUphill.value;
   // def //
-  var def1 = item1def.selectedIndex;
-  var def2 = document.getElementById("item2def");
-  var def3 = document.getElementById("item3def");
-  var def4 = document.getElementById("item4def");
-  var def5 = document.getElementById("item5def");
-  var def6 = document.getElementById("item6def");
+  var def1 = item1def.value;
+  var def2 = item2def.value;
+  var def3 = item3def.value;
+  var def4 = item4def.value;
+  var def5 = item5def.value;
+  var def6 = item6def.value;
   // atk //
-  var atk1 = document.getElementById("item1atk");
-  var atk2 = document.getElementById("item2atk");
-  var atk3 = document.getElementById("item3atk");
-  var atk4 = document.getElementById("item4atk");
-  var atk5 = document.getElementById("item5atk");
-  var atk6 = document.getElementById("item6atk");
-
+  var atk1 = item1atk.value;
+  var atk2 = item2atk.value;
+  var atk3 = item3atk.value;
+  var atk4 = item4atk.value;
+  var atk5 = item5atk.value;
+  var atk6 = item6atk.value;
 
   document.getElementById("test").innerHTML = "Item 1 is " + def1 + " and " + atk1 + ".";
-
+  document.getElementById("test2").innerHTML = "Flying is " + flying + " and uphill is " + uphill + ".";
   // deselect everything at the end
   for(i = 0; i < 6; i++) {
-  document.getElementById("atk" + i).selectedIndex = "-1";
+  document.getElementById('atk' + i).selectedIndex = "-1";
 
-  document.getElementById("def" + i).selectedIndex = "-1";
+  document.getElementById('def' + i).selectedIndex = "-1";
               }
-}
+            }
